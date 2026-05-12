b700a851ccb3e6125f4583180ceadf33a879651a
DuckNellz <shachiorange@gmail.com>
2026-05-12 09:34:19 -0400

    Initial Commit

---
+++ HikeUrban/HikeUrbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeUrbanApp.swift
+//  HikeUrban
+//
+//  Created by Dornell Mister on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HikeUrbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikeUrbanTests/HikeUrbanTests.swift
@@ -0,0 +1,19 @@
+//
+//  HikeUrbanTests.swift
+//  HikeUrbanTests
+//
+//  Created by Dornell Mister on 5/12/26.
+//
+
+import Testing
+@testable import HikeUrban
+
+struct HikeUrbanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
